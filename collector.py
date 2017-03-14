df2c449e27019353c2269216a00ce19f0f293cc7
Teemu Kinnunen <teemu.kinnune@futurice.comn>
2017-03-14 15:21:28 +0200

    Added CLI parameters + now it stores csv files with date range in the filename

--- collector.py
+++ collector.py
@@ -5,22 +5,58 @@ This script predicts/detects hate speech for new messages
 
 '''
 import requests
-
+import argparse
 import sys
 from io import StringIO
+import datetime
+import os
 
 import pandas as pd
 
 sys.path.append('confs')
 import hiit_collector
 
-url = 'http://' + hiit_collector.username + ':' + hiit_collector.password + '@' + hiit_collector.hostname + '/tcat/api/querybin.php'
-#TODO: Set startdate and enddate more nicely
-r = requests.get(url,
-                 params = {'resource': 'querybin/tweets',
-                           'action' : 'tweet-export',
-                           'startdate' : '2017-01-01 00:00:00 utc',
-                           'enddate' : '2017-03-09 23:59:00 utc',
-                           'bin' : 'Kuntavaalit' } )
-# TODO: Store data to database
-open('data/incoming.csv', 'w').write(r.text)
+def main(argv):
+    # Parse inputs
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--user', help='Username')
+    parser.add_argument('--password', help='Password')
+    parser.add_argument('--hostname', help='Hostname')
+    parser.add_argument('--startdate', help='Startdate')
+    parser.add_argument('--enddate', help='Startdate')
+
+    args = parser.parse_args(argv)
+
+    if args.user is None:
+        args.user = hiit_collector.username
+    if args.password is None:
+        args.password = hiit_collector.password
+    if args.hostname is None:
+        args.hostname = hiit_collector.hostname
+    if args.startdate is None:
+        args.startdate = datetime.datetime.now().strftime('%Y-%m-%d 00:00:00 utc')
+    if args.enddate is None:
+        args.enddate = datetime.datetime.now().strftime('%Y-%m-%d 23:59:59 utc')
+
+    # Get the data
+    url = 'http://' + args.user + ':' + args.password + '@' + args.hostname + '/tcat/api/querybin.php'
+    r = requests.get(url,
+                     params = {'resource': 'querybin/tweets',
+                               'action' : 'tweet-export',
+                               'startdate' : args.startdate,
+                               'enddate' : args.enddate,
+                               'bin' : 'Kuntavaalit' } )
+
+    # Store results
+    # TODO: Store data to database
+    outfile = os.path.join('data/', 'incoming',
+                           args.startdate.replace(' ', '_') + '-' +
+                           args.enddate.replace(' ', '_') + '.csv')
+    if not os.path.exists(os.path.dirname(outfile)):
+        os.makedirs(os.path.dirname(outfile))
+    open(outfile, 'w').write(r.text)
+
+
+#
+if __name__ == "__main__":
+    main(sys.argv[1:])
