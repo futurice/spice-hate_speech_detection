2d8683adccea8d4384e4a40640585fdc574c343f
Teemu Kinnunen <teemu.kinnune@futurice.comn>
2017-03-16 10:43:45 +0200

    Added hate words counter

--- predict.py
+++ predict.py
@@ -21,8 +21,37 @@ import fasttext
 # Import our custom scripts
 sys.path.append('libs/')
 import textfeatures
+import texttools # For hatewords stemming
 import fileio
 import classification
+
+class HateWordDetector:
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.hatewords = []
+        self.stemmed_hatewords = []
+        self.stemming2original = []
+
+        self.load_hatewords(filename)
+
+    def load_hatewords(self, filename):
+
+        self.hatewords = open(filename, 'r').read().splitlines()
+        self.stemmed_hatewords, self.stemming2original = texttools.stemming_messages_snowball(self.hatewords)
+
+    def detect_hatewords(self, message):
+
+        stemmed_message, stemming2original = texttools.stemming_message_snowball(message, self.stemming2original.copy())
+        n_hate_words = 0
+        hate_words_in_message = []
+        for word in stemmed_message.split():
+            if word in self.stemmed_hatewords:
+                n_hate_words += 1
+                hate_words_in_message.append(stemming2original[word])
+
+        return n_hate_words, hate_words_in_message
+
 
 def main(argv):
     # Parse inputs
@@ -47,6 +76,9 @@ def main(argv):
     # Load the preditor model
     clf = joblib.load(args.predictor)
 
+    # Load HateWords counter
+    hwd = HateWordDetector('data/vihasanat.txt')
+
     filenames = glob.glob(os.path.join(args.inputdir, '*.csv'))
     for filename in filenames:
         # Load new messages
@@ -62,7 +94,13 @@ def main(argv):
         pred = clf.predict(x)
         score = clf.predict_proba(x)[:,1]
 
+        # Compute the number of hatewords in each message
+        n_hate_words = np.zeros(len(messages))
+        for i, message in enumerate(messages):
+            n_hate_words[i], _ = hwd.detect_hatewords(message)
+
         # Update dataframe
+        df['n_hate_words'] = n_hate_words
         df['predicted_label'] = pred
         df['prediced_score'] = score
 
