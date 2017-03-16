a572f379fc4f8c250d51aec90c33f9f17a3af395
Teemu Kinnunen <teemu.kinnune@futurice.comn>
2017-03-16 09:48:51 +0200

    Added Naive Bayes classifier to the benchmark

--- benchmarks/run_roc_benchmark.py
+++ benchmarks/run_roc_benchmark.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_curve
 from sklearn.ensemble import RandomForestClassifier as RF
 from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
 
 import fasttext
 
@@ -18,6 +19,8 @@ sys.path.append('libs/')
 import textfeatures
 import fileio
 import classification
+
+methods = ('FT+ GNB', 'FT + RF', 'FT + SVM', 'BOW + GNB', 'BOW + MNB', 'BOW + RF', 'BOW + SVM')
 
 pylab.ion()
 pylab.figure()
@@ -28,9 +31,17 @@ y = np.array(y)
 #
 # FastText features
 #
-model = fasttext.load_model('data/tweets_skipgram.bin')
+model = fasttext.load_model('../fastText/data/wiki.fi.bin')
 x = textfeatures.fasttext_bag_of_means(messages, model)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+## Naive Bayes
+# Train, predict and evaluate
+clf = GaussianNB().fit(x_train, y_train)
+score = clf.predict_proba(x_test)
+fpr, tpr, _ = roc_curve(y_test==1, score[:,1])
+# Plot
+pylab.plot(fpr, tpr)
 
 ## RF
 # Train, predict and evaluate
@@ -54,6 +65,21 @@ pylab.plot(fpr, tpr)
 x = textfeatures.bag_of_words(messages, model=None, weighting='tfidf')
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
+## Naive Bayes
+# Train, predict and evaluate
+clf = GaussianNB().fit(x_train.toarray(), y_train)
+score = clf.predict_proba(x_test.toarray())
+fpr, tpr, _ = roc_curve(y_test==1, score[:,1])
+# Plot
+pylab.plot(fpr, tpr)
+
+# Train, predict and evaluate
+clf = MultinomialNB().fit(x_train.toarray(), y_train)
+score = clf.predict_proba(x_test.toarray())
+fpr, tpr, _ = roc_curve(y_test==1, score[:,1])
+# Plot
+pylab.plot(fpr, tpr)
+
 ## RF
 # Train, predict and evaluate
 clf = RF().fit(x_train, y_train)
@@ -70,7 +96,9 @@ fpr, tpr, _ = roc_curve(y_test==1, score[:,1])
 # Plot
 pylab.plot(fpr, tpr)
 
-pylab.legend(('FT + RF', 'FT + SVM', 'BOW + RF', 'BOW + SVM'))
+pylab.legend(methods)
 pylab.xlabel('FPR')
-pylab.ylabel('TPR'
+pylab.ylabel('TPR')
+pylab.grid()
+pylab.axis([0,1,0,1])
 pylab.waitforbuttonpress()
